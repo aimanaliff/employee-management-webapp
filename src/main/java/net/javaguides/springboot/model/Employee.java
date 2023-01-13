417e7c069091134b15e5dc4a048b200c6b671eed
marsyaaa <marsyaamelia00@gmail.com>
2023-01-14 03:12:17 +0800

    add overtime pay

--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot.model;
 
+import java.text.DecimalFormat;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -41,6 +43,18 @@ public class Employee {
 
 	@Column(name = "bonus")
 	private double bonus;
+
+	@Column(name = "ot_hours_working")
+	private int otHoursWorking;
+
+	@Column(name = "ot_hours_rest")
+	private int otHoursRest;
+
+	@Column(name = "ot_hours_public")
+	private int otHoursPublic;
+
+	@Column(name = "ot_pay_total")
+	private double otPayTotal;
 
 	@Column(name = "net_salary")
 	private double netSalary;
@@ -98,18 +112,73 @@ public class Employee {
 		this.bonus = bonus;
 		calculateNetSalary();
 	}
+	public int getOtHoursWorking() {
+		return otHoursWorking;
+	}
+	public void setOtHoursWorking(int otHoursWorking) {
+		this.otHoursWorking = otHoursWorking;
+		calculateNetSalary();
+	}
+	public int getOtHoursRest() {
+		return otHoursRest;
+	}
+	public void setOtHoursRest(int otHoursRest) {
+		this.otHoursRest = otHoursRest;
+		calculateNetSalary();
+	}
+	public int getOtHoursPublic() {
+		return otHoursPublic;
+	}
+	public void setOtHoursPublic(int otHoursPublic) {
+		this.otHoursPublic = otHoursPublic;
+		calculateNetSalary();
+	}
+	public double getOtPayTotal() {
+		return otPayTotal;
+	}
 	public double getNetSalary() {
 		return netSalary;
 	}
 
+	// bonus, 11% EPF deduction, 0.5% SOCSO deduction, 0.2% EIS deduction, OT pay according to hours
 	public void calculateNetSalary() {
+		DecimalFormat df = new DecimalFormat("#.##");    
+
 		basicSalary = getBasicSalary();
+		bonus = getBonus();
+		otHoursWorking = getOtHoursWorking();
+		otHoursRest = getOtHoursRest();
+		otHoursPublic = getOtHoursPublic();
 		
-		// bonus addition, 11% EPF deduction, 0.5% SOCSO deduction, 0.2% EIS deduction
 		epf = 0.11 * basicSalary;
 		socso = 0.005 * basicSalary;
 		eis = 0.002 * basicSalary;
 
-		netSalary = basicSalary + getBonus() - epf - socso - eis;
+		// we assume that all employees are entitled for OT pay
+		double dailyPay = Double.valueOf(df.format(basicSalary/26));
+		double hourlyPay = Double.valueOf(df.format(dailyPay/8));
+
+		double otPayWorking = hourlyPay * otHoursWorking * 1.5;
+		double otPayRest = 0;
+		double otPayPublic = 0;
+
+		if(otHoursRest > 0 && otHoursRest <= 4) {
+			otPayRest = dailyPay * 0.5;
+		} else if(otHoursRest > 4 && otHoursRest <= 8) {
+			otPayRest = dailyPay;
+		} else if(otHoursRest > 8) {
+			double otHoursRestBal = otHoursRest - 8;
+			otPayRest = dailyPay + (hourlyPay * otHoursRestBal * 2);
+		}
+
+		if(otHoursPublic > 0 && otHoursPublic <= 8) {
+			otPayPublic = dailyPay * 2;
+		} else if(otHoursPublic > 8) {
+			double otHoursPublicBal = otHoursPublic - 8;
+			otPayPublic = dailyPay + (hourlyPay * otHoursPublicBal * 3);
+		}
+
+		otPayTotal = Double.valueOf(df.format(otPayWorking + otPayRest + otPayPublic));
+		netSalary = Double.valueOf(df.format(basicSalary + bonus - epf - socso - eis + otPayTotal));
 	}
 }
