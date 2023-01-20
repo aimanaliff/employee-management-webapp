38cb3b91c0ef438dabb4c9c456f5a167d3f9914a
Aliff Aiman <aliffaimanridzuan@gmail.com>
2023-01-21 02:39:44 +0800

    add search bar
    things done:
    added search bar and submit button to index.html
    added the relevant method in the EmployeeController and EmployeeControllerImplementation
    
    things to do:
    mapping method in controller to the search bar in index.html
    look into why query not going in for keyword in EmployeeRepository

--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,11 +1,18 @@
 package net.javaguides.springboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import net.javaguides.springboot.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "SELECT * FROM Employee e WHERE e.firstName LIKE %:keyword%", nativeQuery = true)
+    List<Employee> searchByKeyword(@Param("keyword") String keyword);
 
 }
